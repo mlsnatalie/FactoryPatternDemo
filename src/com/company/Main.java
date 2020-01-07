83ea18f6f2352a897db5575ce2a362ccc2d191b7
mlsnatalie <mlsnatalie@163.com>
2020-01-07 11:24:48 +0800

    观察者模式

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 import com.company.abstractFactory.AbstractFactoryPatternDemo;
 import com.company.build.BuilderPatternDemo;
 import com.company.factory.FactoryPattern;
+import com.company.observer.ObserverPatternDemo;
 
 public class Main {
 
     public static void main(String[] args) {
 //        new FactoryPattern(); //工厂模式
 //        new AbstractFactoryPatternDemo(); //抽象工厂模式
-        new BuilderPatternDemo(); //建造者模式
+//        new BuilderPatternDemo(); //建造者模式
+        new ObserverPatternDemo();//观察者模式
     }
 }
---
+++ src/com/company/observer/ObserverPatternDemo.java
@@ -0,0 +1,17 @@
+package com.company.observer;
+
+public class ObserverPatternDemo {
+
+    public ObserverPatternDemo() {
+        Subject subject = new Subject();
+
+        new BinaryObserver(subject);
+        new OctalObserver(subject);
+        new HexaObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+    }
+}
